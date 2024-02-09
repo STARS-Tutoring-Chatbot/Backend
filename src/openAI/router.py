6f193b5c6ca65ccb4cf51a51900f8958c53c3494
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-02-09 14:17:33 -0500

    chat gpt commit

--- src/openAI/router.py
+++ src/openAI/router.py
@@ -1,6 +1,7 @@
 from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 from fastapi import APIRouter
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
@@ -8,6 +9,9 @@ load_dotenv(dotenv_path)
 
 api_key = os.getenv('API_KEY')
 debug_mode = os.getenv('DEBUG')
+client = OpenAI(
+  api_key=os.getenv('OPEN_AI_DEV_KEY'),
+)
 
 router = APIRouter()
 
@@ -23,8 +27,12 @@ def getSupabaseClient():
 
 @router.get("/testOpenAI")
 def testingOpenAI():
-  pass
-
-
-
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a unhelpful assistant by responding in broken english. However, you have a 1/2 probability of being literally a USMC drill instructor."},
+      {"role": "user", "content": "Hello!"},
+    ],
+  )
+  return completion
 
